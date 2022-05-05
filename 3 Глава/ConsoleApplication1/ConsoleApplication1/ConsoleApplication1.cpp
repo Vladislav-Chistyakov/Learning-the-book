ec6ebc4588bbad73408f36aeb0b5b78b87f6518a
Vladislav_Chistyakov <vladischis@gmail.com>
2022-05-05 21:57:23 +0500

    chapter 3

---
+++ 3 Глава/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	printf("Введите три числа: ");
+	scanf_s("%d %d %d", &a, &b, &c);
+	if ((a % 2) + (b % 2) == 1) {
+		printf("Какое-то число чётное");
+	}
+	return 0;
+}
