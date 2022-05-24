c32252330ef41effeb277af6eecb4f10527a8a24
Vladislav_Chistyakov <vladischis@gmail.com>
2022-05-24 17:44:33 +0500

    chapter 3

---
+++ Глава 3/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a, b, c, d, sumLeft, sumRight;
+	scanf_s("%d", &a);
+
+	b = ((a - a % 100000) / 100000) + ((a % 100000) / 10000) + ((a % 10000) / 1000);
+	c = ((a % 1000) / 100) + ((a % 100) / 10) + ((a % 10) / 1);
+
+	if (b == c) {
+		printf("Вы победитель");
+	}
+	else {
+		printf("Вы проиграли");
+	}
+
+
+
+
+
+
+	return 0;
+}
