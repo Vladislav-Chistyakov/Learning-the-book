ad2e2d9b58f7dfc06f31855a3c06381172703ca4
Vladislav_Chistyakov <vladischis@gmail.com>
2022-05-25 14:58:24 +0500

    chapter 4

---
+++ Глава 4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a, c, *b;
+	b = &a;
+	scanf_s("%d %d", b, &c);
+
+	if (a > c) {
+		for (int i = 1; a != c; i++) {
+			a = a - c;
+			if (a == c) {
+				printf("наибольший делитель - %d", *b);
+				break;
+			}
+		}
+	}
+	else {
+		for (int i = 1; c != a; i++) {
+			c = c - a;
+			if (c == a) {
+				printf("наибольший делитель - %d", c);
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
