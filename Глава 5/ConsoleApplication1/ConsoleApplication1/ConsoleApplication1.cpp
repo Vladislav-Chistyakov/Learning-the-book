50d03921257e20e8149c8f3a6f6b0c94f1275212
Vladislav_Chistyakov <vladischis@gmail.com>
2022-05-27 16:23:49 +0500

    chapter 5 and chapter 6

---
+++ Глава 5/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <locale.h>
+// Объявляем функцию
+int factorial(int n);
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a;
+	scanf_s("%d", &a);
+
+	factorial(a);
+	return 0;
+}
+
+int factorial(int n) {
+	int a, b, c;
+	n = 100;
+
+	for (int i = 1; i < n; i++) {
+		for (int j = 2; j <= i; j++) {
+			int p = 0;
+			if (i % j == 0 && i != j) {
+				p = 1;
+				break;
+			}
+			if (i == j) {
+				if (p != 1) {
+					printf("Простое число %d\n", i);
+				}
+			}
+		}
+	}
+
+	return n;
+}
+
+
+
---
+++ Глава 6/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	FILE* inFile, * outFile;
+	int min, max, tmp;
+	fopen_s(&inFile, "input.txt", "r");
+	fopen_s(&outFile, "output.txt", "w");
+	fscanf_s(inFile, "%d", &min);
+	max = min;
+	while (!feof(inFile)) {
+		if (!feof(inFile)) {
+			printf("XS - 1");
+		}
+		else {
+			printf("XS - 2");
+		} 
+		if (fscanf_s(inFile, "%d", &tmp) != -1) {
+			if (max < tmp) max = tmp;
+			if (min > tmp) min = tmp;
+		}
+	}
+	printf("Min = %d, max = %d\n", min, max);
+	fprintf(outFile, "Min = %d, max = %d\n", min, max);
+	fclose(inFile);
+	fclose(outFile);
+	return 0;
+}
