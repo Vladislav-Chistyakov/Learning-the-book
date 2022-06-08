894f8dce21ef284b501c316102ab53db6099bfa3
Vladislav_Chistyakov <vladischis@gmail.com>
2022-06-08 19:22:49 +0500

    chapter 7 and 8

---
+++ Глава 7/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+const int N = 10;
+void printMas(int mas[N][N]) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+			printf("%4d", mas[i][j]);
+		printf("\n");
+	}
+	printf("\n");
+}
+void generateRandomMas(int mas[N][N]) {
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			mas[i][j] = rand() % 101;
+}
+ int main() {
+	const int N = 10;
+	int mas1[N][N], mas2[N][N], result[N][N];
+	srand(time(0));
+	generateRandomMas(mas1);
+	generateRandomMas(mas2);
+	printMas(mas1);
+	printMas(mas2);
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			result[i][j] = mas1[i][j] + mas2[i][j];
+	printMas(result);
+	return 0;
+}
+
+
+
+
+
+
+
+
---
+++ Глава 8/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	return 0;
+}
+
+
+
+
+
+
