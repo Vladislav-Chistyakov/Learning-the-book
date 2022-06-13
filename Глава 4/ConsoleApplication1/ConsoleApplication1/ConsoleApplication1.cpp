ab6bf4f8e215c45b8808125d8ac9ca85a2b9d06c
Vladislav_Chistyakov <vladischis@gmail.com>
2022-06-13 09:25:11 +0500

    Repetition of Chapter 4

--- Глава 4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Глава 4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,28 +3,48 @@
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int a, c, *b;
-	b = &a;
-	scanf_s("%d %d", b, &c);
+	int a, b, c, sum, max, min;
+	// Задача 1
+	// Вывод всех делителей числа
 
-	if (a > c) {
-		for (int i = 1; a != c; i++) {
-			a = a - c;
-			if (a == c) {
-				printf("наибольший делитель - %d", *b);
-				break;
-			}
+	printf("Введите число: ");
+	scanf_s("%d", &a);
+	printf("Делители числа %d: ", a);
+	for (int i = 1; i <= a; i++) {
+		if (a % i == 0) {
+			printf("%d   ", i);
 		}
 	}
-	else {
-		for (int i = 1; c != a; i++) {
-			c = c - a;
-			if (c == a) {
-				printf("наибольший делитель - %d", c);
-				break;
-			}
+	printf("\n");
+
+	// Задача 2
+	// Вывод одинаковых делителей двух введёных чисел
+
+	printf("Введите два целых положительных числа: ");
+	scanf_s("%d %d", &a, &b);
+	printf("Делители числа %d и %d: ", a, b);
+	for (int i = 1; i <= a && i <= b; i++) {
+		if (a % i == 0 && b % i == 0) {
+			printf("%d  ", i);
 		}
 	}
+	printf("\n");
+
+	// Задача 3
+	// вывод простых чисел от заданого числа
+	int N, i, j, isPrime;
+	printf("Введите число: ");
+	scanf_s("%d", &N);
+	printf("Простые числа от 1 до %d: ", N);
+	for (i = 1; i <= N; i++) {
+		isPrime = 1;
+		for (j = 2; j < i; j++) {
+			if (i % j == 0) isPrime = 0;
+		}
+		if (isPrime == 1) printf("%d ", i);
+	}
+	printf("\n");
+
 
 	return 0;
 }
